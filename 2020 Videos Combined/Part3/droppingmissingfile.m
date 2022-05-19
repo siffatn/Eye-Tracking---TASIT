200582d7aa192e86c2cfa8811cebb94de7f00190
iffat002 <iffat002@umn.edu>
2022-05-19 16:13:10 -0400

    Dropping the files with missing data

--- 2020 Videos Combined/Part3/droppingmissingfile.m
+++ 2020 Videos Combined/Part3/droppingmissingfile.m
@@ -3,12 +3,13 @@ clear all;
 datadir='C:\Users\siffa\Desktop\trial data\Video9';
 path = datadir;
 participants= dir(fullfile(datadir,'*.mat'));
+threshold = 0.9;
 k=1;
 for i = 1 : length(participants)    
    fname=participants(i).name;
    part_num = str2double(fname(18:22));
    load(strcat(path,'\',fname));  
-   if (sum(isnan(data(:,12)))/length(data))>=0.9      
+   if (sum(isnan(data(:,12)))/length(data))>=threshold     
        participant_drop (k) = part_num;
        k = k+1;
    end
